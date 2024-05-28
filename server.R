66bb3f795940bae9d8cfc39d761041cd132ddc38
ben-richmond-ricardo <ben.richmond@ricardo.com>
2024-05-29 00:38:45 +0100

    removed commented out lines

--- server.R
+++ server.R
@@ -56,51 +56,5 @@ shinyServer(function(input, output) {
     source("src/server/server_partnership.R",local = TRUE)
     
 
-    # # cut down stats for visualising
-    # batting_summary <- DBI::dbGetQuery(
-    #     conn,
-    #     "SELECT
-    # r.opposition,
-    # r.match_date,
-    # position,
-    # batsman_name,
-    # batsman_id,
-    # bd.clean_dismissal,
-    # runs,
-    # balls,
-    # fours,
-    # sixes
-    # FROM batting b
-    # left join batting_dismissals bd on b.how_out = bd.pc_dismissal
-    # left join results r on b.match_id = r.id;"
-    # )
-    # 
-    # # overall Bowling Stats
-    # bowling_all_detail <- DBI::dbGetQuery(
-    #     conn,
-    #     "SELECT
-    #     r.opposition,
-    #     r.match_date,
-    #     b.*
-    #     FROM bowling b
-    #     left join results r on b.match_id = r.id;"
-    # )
-    # 
-    # # cut down stats for visualising
-    # bowling_summary <- DBI::dbGetQuery(
-    #     conn,
-    #     "SELECT
-    # r.opposition,
-    # r.match_date,
-    # bowler_name,
-    # bowler_id,
-    # ball_count,
-    # maidens,
-    # runs,
-    # wickets,
-    # wides,
-    # no_balls
-    # FROM bowling b
-    # left join results r on b.match_id = r.id;"
-    # )
+    
 })
