d58377a900ac8bb2f6a6d64bc819d4442b3e8738
ben-richmond-ricardo <ben.richmond@ricardo.com>
2024-06-09 22:21:47 +0100

    added in bowling stats by batting position

--- app/src/server/server_bowling_detailed.R
+++ app/src/server/server_bowling_detailed.R
@@ -10,3 +10,4 @@ detailed_bowling_import <- read_csv(bowling_detail_fp) |>
   filter(grepl("Harwell.*",`Bowling Team`))
 
 source("src/server/bowling_detailed/bowling_by_end.R",local = TRUE)
+source("src/server/bowling_detailed/bowling_by_batting_position.R",local = TRUE)
